2d755c1e97f1d87e2be42e23d8ec1eb07167dde8
melissa-tan10 <69818577+melissa-tan10@users.noreply.github.com>
2020-09-14 11:33:20 -0400

    Add files via upload

---
+++ mtanp1.py
@@ -0,0 +1,10 @@
+# Melissa Tan
+# September 13, 2020
+# 
+# This program outputs a welcome message and calculates the answer to an equation given variable x.
+
+x = 1
+result = (4 * x + 3)/0.2
+print("Hello, welcome to my Python program! My name is Melissa.")
+print("The value of x is", x)
+print("The result of (4x + 3)/0.2 is", result)
